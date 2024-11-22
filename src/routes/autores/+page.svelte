9f4d30e38da887c8347d45915536d828ac61281f
Wagner <mobile.costa@gmail.com>
2024-11-21 21:15:56 -0300

    Commit Projeto

---
+++ src/routes/autores/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import ItemAutor from "../../components/autor/ItemAutor.svelte";
+  import Titulo from "../../components/shared/Titulo.svelte";
+  import autores from "../../constants/autores";
+</script>
+
+<div class="flex flex-col gap-5">
+  <Titulo
+    principal="Autores"
+    secundario="Conheça os autores que escrevem as histórias que você ama"
+  />
+  <div class="grid grid-cols-2 gap-5">
+    {#each autores as autor}
+      <ItemAutor {autor} />
+    {/each}
+  </div>
+</div>
